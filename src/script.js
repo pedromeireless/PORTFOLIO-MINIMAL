7959e50b755cf99eca7b19286ba81aa1226054b3
pedromeireless <pedrohcmeirelles@gmail.com>
2022-06-23 10:57:55 -0300

    js file

---
+++ src/script.js
@@ -0,0 +1,4 @@
+const closeBtn = document.querySelector('.close-btn')
+const menu = document.querySelector('.menu')
+const menuHamburguer = document.querySelector('.menu-hamburguer')
+const header = document.querySelector('header')
